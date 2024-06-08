78258b825a7389be3d7002f0fae7bb541312f54f
Mike Peiman <mikepeiman@gmail.com>
2024-06-08 18:41:07 -0400

    first commit, bg image, added tailwindcss and enhanced-img module

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+		background-image: url(dall-e3-mosaic-bento-bg-0.jpg);
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+	}
+</style>
